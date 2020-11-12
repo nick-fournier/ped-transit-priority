7cda3c6f34201a351af53e0e1a4e6927295a1183
nick-fournier <nichfournier@gmail.com>
2020-11-12 06:10:01 +0000

    updated legend

--- analysis/mfdplots.R
+++ analysis/mfdplots.R
@@ -197,18 +197,18 @@ fun.timeflow_dag <- function(q) {
 
 
 #### Flow-density function
-flabs = c("Bi-parabolic","Bi-linear","Parabolic-Exponential")
+flabs = c("Parabolic","Bi-linear","Parabolic-Exponential")
 
 plots[['flowdensity']] <- ggplot(data.frame(k = c(0, 40)), aes(k)) + 
-  stat_function(fun = fun.flowdensity_bipara1, linetype = 1, aes(color = "para")) +
-  stat_function(fun = fun.flowdensity_bipara2, linetype = 2, aes(color = "para")) +
+  stat_function(fun = fun.flowdensity_bipara1, linetype = 1, aes(color = "bipara")) +
+  stat_function(fun = fun.flowdensity_bipara2, linetype = 2, aes(color = "bipara")) +
   stat_function(fun = fun.flowdensity_dag1, linetype = 1, aes(color = "dag")) +
   stat_function(fun = fun.flowdensity_dag2, linetype = 2, aes(color = "dag")) +
   stat_function(fun = fun.flowdensity_green1, linetype = 1, aes(color = "green")) +
   stat_function(fun = fun.flowdensity_green2, linetype = 2, aes(color = "green")) +
   scale_y_continuous("Traffic flow (veh/hr/lane)", labels = scales::comma, breaks = seq(0, 600, by = 100), expand = c(0,0)) +
   scale_x_continuous("Traffic density (veh/km/lane)", limits = c(0, 150), breaks = seq(0, 150, by = 25), expand = c(0,0)) +
-  scale_color_brewer("Traffic Flow Model", palette = "Set1", limits = c("para","dag","green"), labels = flabs) +
+  scale_color_brewer("Traffic Flow Model", palette = "Set1", limits = c("green","dag","bipara"), labels = flabs) +
   #scale_linetype_manual("Traffic Flow Model", values = c(1,2,5), limits = c("para","dag","green"), labels = flabs) +
   coord_cartesian(xlim = c(0,155), ylim = c(0,600)) +
   theme_classic() +
@@ -219,13 +219,13 @@ plots[['flowdensity']] <- ggplot(data.frame(k = c(0, 40)), aes(k)) +
 
 #### Speed-density function
 plots[['speeddensity']] <- ggplot(data.frame(k = c(0, 40)), aes(k)) + 
-  stat_function(fun = function(k) fun.flowdensity_bipara(k)/k, aes(linetype = "para", color = "para")) +
+  stat_function(fun = function(k) fun.flowdensity_bipara(k)/k, aes(linetype = "bipara", color = "bipara")) +
   stat_function(fun = function(k) fun.flowdensity_dag(k)/k, aes(linetype = "dag", color = "dag"), alpha = 0.5) +
   stat_function(fun = function(k) fun.flowdensity_green(k)/k, aes(linetype = "green", color = "green"), alpha = 0.5) +
   scale_y_continuous("Traffic speed (km/hr)", labels = scales::comma, breaks = seq(0, 100, by = 25), expand = c(0,0)) +
   scale_x_continuous("Traffic density (veh/km/lane)", limits = c(0, 150), breaks = seq(0, 150, by = 25), expand = c(0,0)) +
-  scale_color_brewer("Traffic Flow Model", palette = "Set1", limits = c("para","dag","green"), labels = flabs) +
-  scale_linetype_manual("Traffic Flow Model", values = c(1,2,5), limits = c("para","dag","green"), labels = flabs) +
+  scale_color_brewer("Traffic Flow Model", palette = "Set1", limits = c("green","dag","bipara"), labels = flabs) +
+  #scale_linetype_manual("Traffic Flow Model", values = c(1,2,5), limits = c("green","dag","bipara"), labels = flabs) +
   coord_cartesian(xlim = c(0,155), ylim = c(0,120)) +
   theme_classic() +
   theme(legend.position = c(0.75,0.5), 
@@ -245,7 +245,7 @@ plots[['timeflow']] <- ggplot(data.frame(q = c(0, 2*q_c)), aes(q)) +
   scale_y_continuous("Travel time over 10km (mins)", labels = scales::comma, breaks = seq(0, 200, by = 25), expand = c(0,0)) +
   scale_x_continuous("Traffic flow (veh/hr)", limits = c(0, 1.5*q_c), breaks = seq(0, 2*q_c, by = 100), expand = c(0,0)) +
   scale_color_brewer("Traffic Flow Model", palette = "Set1", limits = c("bipara","dag","paraexp"), labels = flabs) +
-  scale_linetype_manual("Traffic Flow Model", values = c(2,5,1), limits = c("bipara","dag","paraexp"), labels = flabs) +
+  #scale_linetype_manual("Traffic Flow Model", values = c(2,5,1), limits = c("bipara","dag","paraexp"), labels = flabs) +
   coord_cartesian(xlim = c(0,750), ylim = c(0,150)) +
   theme_classic() +
   theme(legend.position = c(0.25,0.5), 
