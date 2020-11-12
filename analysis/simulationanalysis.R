b526960cfb8f6ef585bef1a67ba6efabe95ca6d3
nick-fournier <nichfournier@gmail.com>
2020-11-12 10:27:13 -0800

    revised optimal parameter plot to have % axis labels

--- analysis/simulationanalysis.R
+++ analysis/simulationanalysis.R
@@ -248,8 +248,8 @@ plots[['optimal']] <- ggplot(data = plotmat, aes(x = gamma/R, y = tau/R)) +
   annotate("text", x = 3/4, y = 1/10, label = "Area under diagonal\ndenotes unrealistic\nregion where:", hjust=0.5, vjust=-0.25) +
   annotate("text", x = 3/4, y = 1/10, label = "tau < gamma", hjust=0.5, parse = T, vjust=0.25) +
   
-  scale_x_continuous(expression("Pedestrian zone size, "~frac(gamma,R)), expand = c(0,0), breaks = seq(0,1,by=0.2)) +
-  scale_y_continuous(expression("Transit priority zone size, "~frac(tau,R)), expand = c(0,0), breaks = seq(0,1,by=0.2)) +
+  scale_x_continuous(expression("Pedestrian zone size, "~frac(gamma,R)), labels = scales::percent,  expand = c(0,0), breaks = seq(0,1,by=0.2)) +
+  scale_y_continuous(expression("Transit priority zone size, "~frac(tau,R)), labels = scales::percent, expand = c(0,0), breaks = seq(0,1,by=0.2)) +
   scale_fill_brewer("Average travel time (hours)", palette = 'Blues', direction = -1,
                     label = c(paste0("<",round(brks,2)[lower]),
                               paste0(round(brks,2)[lower:(upper-2)], " - ",
@@ -264,5 +264,5 @@ plots[['optimal']] <- ggplot(data = plotmat, aes(x = gamma/R, y = tau/R)) +
          panel.grid = element_blank())
 )
 
-#plots[['optimal']]
+# plots[['optimal']]
 
